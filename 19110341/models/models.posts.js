81ff6353e924939e42d9df578d63254c570a08fa
duong le hai <lehaiduongbmt2@gmail.com>
2022-10-04 23:28:31 +0700

    first commit

---
+++ 19110341/models/models.posts.js
@@ -0,0 +1,25 @@
+var posts = [{
+    id: 0,
+    title: 'Hai Duong gio o dau?',
+    content: `Trong lanh tho Viet Nam!!`,
+    comments: [{
+        user: 'Dai Phat',
+        comment: 'Bach khoa HN?'
+    }, {
+        user: 'Van Tinh',
+        comment: `Vin ??`
+    }]
+}, {
+    id: 1,
+    title: 'Dai Phat the nao ?',
+    content: `Dai Phat Dep Trai`,
+    comments: [{
+        user: 'Hai Duong',
+        comment: 'Dung !!'
+    }, {
+        user: 'Van Tinh',
+        comment: 'Co that khong?'
+    }]
+}]
+
+module.exports = posts
